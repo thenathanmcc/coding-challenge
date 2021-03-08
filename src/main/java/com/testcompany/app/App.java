f8d458f1996662ecb4da2fb6e6b1c9ec3176758e
Nathan <natamcculloch@gmail.com>
2021-03-09 12:32:21 +1300

    Added JSON file parsing code

--- src/main/java/com/testcompany/app/App.java
+++ src/main/java/com/testcompany/app/App.java
@@ -1,5 +1,16 @@
 package com.testcompany.app;
 
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Iterator;
+
 /**
  * Application Entry Point
  */
@@ -7,6 +18,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        try (FileReader reader = new FileReader("./data.json"))
+        {
+            // Read in data from JSON file
+            JsonObject obj = (JsonObject) Jsoner.deserialize(reader);
+            JsonArray data = (JsonArray) obj.get("data");
+
+            System.out.println(data);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JsonException e) {
+            e.printStackTrace();
+        }
     }
 }
